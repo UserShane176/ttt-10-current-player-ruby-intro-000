1c1c7594f2053213f5c30e88b9dc3f47808cf5e1
Min-Hsuan Hung <poxuan33@gmail.com>
2018-08-24 12:34:03 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 1
   board.each do |index|
-   if index == ("X" || "O")
+   if index == "X" || index == "O"
    counter += 1
    end
  end
 end
+
